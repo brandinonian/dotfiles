08188570573c4c44049c2f9f8ce40aa3de880567
Brandon Phillips <brandoni.me@gmail.com>
2025-09-20 08:18:31 -0600

    add homebrew script

---
+++ brew/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null
+then
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+else
+	echo "Homebrew already installed, skipping installation"
+fi
